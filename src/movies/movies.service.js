e8f5975a2c9b8941195456d7ead2c3b9565de79b
Liz Niedzwiecki <liz0niedz@gmail.com>
2021-10-07 20:39:44 -0400

    initial commit

---
+++ src/movies/movies.service.js
@@ -0,0 +1,64 @@
+const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
+
+const addCritic = mapProperties({
+    critic_id: "critic.critic_id",
+    preferred_name: "critic.preferred_name",
+    surname: "critic.surname",
+    organization_name: "critic.organization_name",
+  });
+
+function list() {
+    return knex("movies")
+    .select("*")
+    .groupBy('movies.movie_id');
+}
+
+function listActiveMovies() {
+    return knex("movies as m")
+    .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+    .select("m.*")
+    .where({"mt.is_showing": true})
+    .groupBy("m.movie_id")
+    
+}
+
+function read(movieId) {
+    return knex("movies")
+    .select("*")
+    .where({ movie_id: movieId })
+    .groupBy("movies.movie_id")
+    .first()
+
+}
+
+function listTheaters(movie_id) {
+    return knex("movies_theaters as mt")
+    .join("theaters as t", "mt.theater_id", "t.theater_id")
+    .select("*")
+    .where({ "mt.movie_id": movie_id, is_showing: true})
+}
+
+function listReviews(movieId) {
+     return knex("movies as m")
+    .join("reviews as r", "m.movie_id", "r.movie_id")
+    .join("critics as c", "c.critic_id", "r.critic_id")
+    .select("*")
+    .where({ "r.movie_id": movieId})
+    .then((result) => {
+        const movieList = [];
+        result.forEach((item) => {
+            const appendedObject = addCritic(item);
+            movieList.push(appendedObject);
+        })
+        return movieList;
+    })
+}
+
+module.exports = {
+    list,
+    listActiveMovies,
+    read,
+    listTheaters,
+    listReviews,
+  };
